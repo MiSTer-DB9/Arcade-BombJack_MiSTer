e619301e17f17c3e99444177387e88222c912d69
Miguel-T80c <the80scomputers@gmail.com>
2020-02-21 02:37:09 +0100

    Joystick DB9MD Support

--- Arcade-BombJack.sv
+++ Arcade-BombJack.sv
@@ -82,14 +82,22 @@ module emu
 	// 1 - D-/TX
 	// 2..6 - USR2..USR6
 	// Set USER_OUT to 1 to read from USER_IN.
-	input   [6:0] USER_IN,
-	output  [6:0] USER_OUT
+	output	USER_MODE,
+input	[7:0] USER_IN,
+output	[7:0] USER_OUT
 	
 	
 );
 
 assign VGA_F1    = 0;
-assign USER_OUT  = '1;
+//assign USER_OUT  = '1;
+
+wire   joy_split, joy_mdsel;
+wire   [5:0] joy_in = {USER_IN[6],USER_IN[3],USER_IN[5],USER_IN[7],USER_IN[1],USER_IN[2]};
+assign USER_OUT  = |status[31:30] ? {3'b111,joy_split,3'b111,joy_mdsel} : '1;
+assign USER_MODE = |status[31:30] ;
+
+
 assign LED_USER  = ioctl_download;
 assign LED_DISK  = 0;
 assign LED_POWER = 0;
@@ -102,8 +110,9 @@ localparam CONF_STR = {
 	"A.BMBJCK;;",
 	"H0O1,Aspect Ratio,Original,Wide;",
 	"H0O2,Orientation,Vert,Horz;",
-	"O35,Scandoubler Fx,None,HQ2x,CRT 25%,CRT 50%,CRT 75%;",
-	"-;",
+"-;",
+	"OUV,Serial SNAC DB9MD,Off,1 Player,2 Players;",
+"-;",
 	"O7,Demo Sounds,On,Off;",
 	"O89,Lives,3,4,5,2;",
 	"OAB,Bonus,500k,750k;",
@@ -111,7 +120,7 @@ localparam CONF_STR = {
 	"ODE,Enemy num & speed,Easy,Medium,Hard,Insane;",	
 	"OIJ,Bird Speed,Easy,Medium,Hard,Insane;",
 	"OFH,Bonus Life,None,Every 100k,Every 30k,50k only,100k only,50k and 100k,100k and 300k,50k and 100k and 300k;",	
-	"-;",
+"-;",
 	"R0,Reset;",
 	"J1,Jump,Start 1P,Start 2P,Coin;",
 	"jn,A,Start,Select,R;",
@@ -148,10 +157,46 @@ wire  [7:0] ioctl_dout;
 
 wire [10:0] ps2_key;
 
-wire [15:0] joystick_0, joystick_1;
+wire [15:0] joystick_0_USB, joystick_1_USB;
 wire [15:0] joy = joystick_0 | joystick_1;
 
 wire [21:0] gamma_bus;
+
+wire [15:0] joystick_0 = |status[31:30] ? {
+	joydb9md_1[8] | (joydb9md_1[7] & joydb9md_1[4]),// Mode|  Start + B-> 8 * Coin
+	joydb9md_1[11],// _start_2	-> 7 * Z (dummy)
+	joydb9md_1[7], // _start_1	-> 6 * Start
+	joydb9md_1[4], // btn_fire	-> 4 * B
+	joydb9md_1[3], // btn_up	-> 3 * U
+	joydb9md_1[2], // btn_down	-> 2 * D
+	joydb9md_1[1], // btn_left	-> 1 * L
+	joydb9md_1[0], // btn_righ	-> 0 * R 
+	} 
+	: joystick_0_USB;
+
+wire [15:0] joystick_1 =  status[31]    ? {
+	joydb9md_2[8] | (joydb9md_2[7] & joydb9md_2[4]),// Mode |Start + B-> 8 * Coin
+	joydb9md_2[7], // _start_2	-> 7 * Start
+	joydb9md_2[11],// _start_1	-> Z (dummy)
+	joydb9md_2[4], // btn_fire  -> B
+	joydb9md_2[3], // btn_up     -> U
+	joydb9md_2[2], // btn_down   -> D
+	joydb9md_2[1], // btn_left   -> L
+	joydb9md_2[0], // btn_right  -> R 
+	} 
+	: status[30] ? joystick_0_USB : joystick_1_USB;
+
+
+reg [15:0] joydb9md_1,joydb9md_2;
+joy_db9md joy_db9md
+(
+  .clk       ( clk_sys    ), //35-50MHz
+  .joy_split ( joy_split  ),
+  .joy_mdsel ( joy_mdsel  ),
+  .joy_in    ( joy_in     ),
+  .joystick1 ( joydb9md_1 ),
+  .joystick2 ( joydb9md_2 )	  
+);
 
 
 hps_io #(.STRLEN($size(CONF_STR)>>3)) hps_io
@@ -173,8 +218,9 @@ hps_io #(.STRLEN($size(CONF_STR)>>3)) hps_io
 	.ioctl_addr(ioctl_addr),
 	.ioctl_dout(ioctl_dout),
 
-	.joystick_0(joystick_0),
-	.joystick_1(joystick_1),
+	.joystick_0(joystick_0_USB),
+	.joystick_1(joystick_1_USB),
+	.joy_raw({(joydb9md_1[8]&joydb9md_1[6]),joydb9md_1[4:0]}),	
 	.ps2_key(ps2_key)
 );
 
